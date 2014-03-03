f7bf9c822fbe376861b432180a16936c061b40f4
Kostas Antonopoulos <kostas@devpro.gr>
2014-03-03 15:12:29 +0200

    Initial Commit

---
+++ DPBlurView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DPBlurView
+//
+//  Created by Kostas on 3/3/14.
+//  Copyright (c) 2014 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DPBlurView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DPBlurView
+//
+//  Created by Kostas on 3/3/14.
+//  Copyright (c) 2014 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
